81740917b8e13e306d88cc8a1a19e348f5b47c21
whalewalker <mongodbms@gmail.com>
2024-04-22 15:47:14 +0100

    project setup

---
+++ src/main/java/com/linkk/LinkkApplication.java
@@ -0,0 +1,13 @@
+package com.linkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linkk/LinkkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
