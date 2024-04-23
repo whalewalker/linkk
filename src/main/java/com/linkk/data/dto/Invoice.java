4f6d09ac87f9b00eefbded98efaa3ccfa174fadc
whalewalker <mongodbms@gmail.com>
2024-04-23 08:17:53 +0100

    added invoice dto

--- src/main/java/com/linkk/data/dto/Invoice.java
+++ src/main/java/com/linkk/data/dto/Invoice.java
@@ -1,29 +1,19 @@
-package com.linkk.data.model;
+package com.linkk.data.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDate;
 
-@Entity
-@Table(name = "invoices")
-@Schema(description = "Represents an invoice")
+@Schema(description = "Represents an invoice DTO")
 @Data
 public class Invoice {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, unique = true)
     @Schema(description = "The invoice number", example = "INV-0001")
     private String invoiceNumber;
 
-    @Column(nullable = false)
     @Schema(description = "The invoice amount", example = "100.00")
     private Double amount;
 
-    @Column(nullable = false)
     @Schema(description = "The due date of the invoice", example = "2023-05-01")
     private LocalDate dueDate;
 }
